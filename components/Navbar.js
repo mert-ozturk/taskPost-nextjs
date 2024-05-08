634135078b50bf0219f11a31455c7b4d45ad3d33
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2024-05-08 21:07:08 +0300

    islem2

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,29 @@
 'use client'
 
-import { useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   const {data:session} = useSession()
   return (
     <header>
-      
+      <div>
+        <Link href='/'>TaskManager</Link>
+        <div>
+          {session ? (
+            <>
+            <p>{session.user.email}</p>
+            <Link href='/task'>Task</Link>
+            <button onClick={()=>signOut()}>signOut</button>
+            </>
+          ):(
+          <button onClick={()=>signIn()}>SingnIn</button>
+          )
+
+          }
+        </div>
+      </div>
     </header>
   )
 }
