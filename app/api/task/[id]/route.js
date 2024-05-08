9da09bc86d2ca4a01928f397368eab03859714fe
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2024-05-08 21:43:01 +0300

    islem4

--- app/api/task/[id]/route.js
+++ app/api/task/[id]/route.js
@@ -1,3 +1,4 @@
+import db from "@/lib/db"
 import Task from "@/models/Task"
 import { NextResponse } from "next/server"
 
@@ -13,4 +14,30 @@ export async function GET(req,ctx){
     }catch(error){
         return new NextResponse(JSON.stringify(null),{status:500})
     }
+}
+
+
+export async function PUT(req,ctx){
+    await db.connect()
+    const id=ctx.params.id
+    try{
+    const body = await req.json()
+    const updateTask = await Task.findByIdAndUpdate(id,{$set:{...body}},{new:true})
+    return new NextResponse(JSON.stringify(updateTask),{status:200})
+    }catch(error){
+    return new NextResponse(JSON.stringify(null),{status:500})
+    }
+}
+
+
+export async function DELETE(req,ctx){
+    await db.connect()
+    const id=ctx.params.id
+    try{
+     const task = await Task.findByIdAndDelete(id)
+     return new NextResponse(JSON.stringify({msg:"Deleted"}),{status:200})
+    }catch(error){
+     return new NextResponse(JSON.stringify(null),{status:500})
+    }
+
 }
